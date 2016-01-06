26c96d79e634046bd448d51104459587a057f626
nkeeslin <nathan.keesling@gmail.com>
2016-01-06 10:59:39 -0700

    build skeleton app

---
+++ MicrobusinessManager/www/js/app.js
@@ -0,0 +1,92 @@
+(function () {
+    angular.module('app', ['ionic'])
+    .run(run)
+    .config(config);
+
+    function run ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
+            }
+
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        })
+    }
+
+    function config ($stateProvider, $urlRouterProvider) {
+		$stateProvider
+		.state('app', {
+			url: '/app',
+			abstract: true,
+			templateUrl: 'templates/menu.html',
+			controller: 'AppController',
+			controllerAs: 'app'
+		})
+		.state('app.sales', {
+			url: '/sales',
+			views: {
+				'menuContent': {
+					templateUrl: 'templates/sales.html',
+					controller: 'SalesController',
+					controllerAs: 'sales'
+				}
+			}
+		})
+		.state('app.products', {
+			url: '/products',
+			views: {
+				'menuContent': {
+					templateUrl: 'templates/products.html',
+					controller: 'ProductsController',
+					controllerAs: 'products'
+				}
+			}
+		})
+		.state('app.inventory', {
+			url: '/inventory',
+			views: {
+				'menuContent': {
+					templateUrl: 'templates/inventory.html',
+					controller: 'InventoryController',
+					controllerAs: 'inventory'
+				}
+			}
+		})
+		.state('app.expenses', {
+			url: '/expenses',
+			views: {
+				'menuContent': {
+					templateUrl: 'templates/expenses.html',
+					controller: 'ExpensesCtrl',
+					controllerAs: 'expenses'
+				}
+			}
+		})
+		.state('app.reports', {
+			url: '/reports',
+			views: {
+				'menuContent': {
+					templateUrl: 'templates/reports.html',
+					controller: 'ReportsCtrl',
+					controllerAs: 'reports'
+				}
+			}
+		})
+		.state('app.settings', {
+			url: '/settings',
+			views: {
+				'menuContent': {
+					templateUrl: 'templates/settings.html',
+					controller: 'SettingsCtrl',
+					controllerAs: 'settings'
+				}
+			}
+		});
+
+		// if none of the above states are matched, use this as the fallback
+		$urlRouterProvider.otherwise('app/sales');
+	}
+})();
---
+++ MicrobusinessManager/www/js/controllers.js
@@ -0,0 +1,39 @@
+(function () {
+    angular.module('app')
+    .controller('AppController', AppController)
+    .controller('SalesController', SalesController)
+    .controller('ProductsController', ProductsController)
+    .controller('InventoryController', InventoryController)
+    .controller('ExpensesController', ExpensesController)
+    .controller('ReportsController', ReportsController)
+    .controller('SettingsController', SettingsController);
+
+    function AppController () {
+
+    }
+
+    function SalesController () {
+
+    }
+
+    function ProductsController () {
+
+    }
+
+    function InventoryController () {
+
+    }
+
+    function ExpensesController () {
+
+    }
+
+    function ReportsController () {
+
+    }
+
+    function SettingsController () {
+
+    }
+})();
+
