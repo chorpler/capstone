e74ce632a5abea9f2e1304eadd624c864f18df9b
nkeeslin <nathan.keesling@gmail.com>
2016-01-06 15:25:55 -0700

    Inventory Page
    Matches the functionality of our mockups.  Still doesn't look great, we
    need to decide on a style guide

--- MicrobusinessManager/www/js/controllers.js
+++ MicrobusinessManager/www/js/controllers.js
@@ -20,15 +20,83 @@
 
     }
 
-    function InventoryController () {
+    function InventoryController ($scope, $ionicModal) {
         var vm = this;
 
-        vm.items = [{
-            name: 'Soda',
-            quantity: '100',
-            costPerUnit: '1.20',
-            totalCost: '120'
-        }]
+        vm.items = [];
+        vm.activeItem = null;
+        vm.totalAssets = 0;
+        vm.editModal = null;
+
+        vm.editItem = editItem;
+        vm.save = save;
+        vm.cancel = cancel;
+        vm.addNewItem = addNewItem;
+
+        var tempItem = null;
+
+        function init () {
+            vm.items = [{
+                id: '1',
+                name: 'Soda',
+                quantity: 100,
+                unitCost: 1.20
+            }, {
+                id: '2',
+                name: 'Chocolate',
+                quantity: 150,
+                unitCost: 1.00
+            }];
+
+            vm.totalAssets = vm.items.reduce(function (previousValue, currentItem) {
+                return previousValue + (currentItem.quantity * currentItem.unitCost);
+            }, 0);
+
+            $ionicModal.fromTemplateUrl('templates/inventoryEditModal.html', {
+                scope: $scope,
+                animation: 'slide-in-up'
+            }).then(function (modal) {
+                vm.editModal = modal;
+            });
+        }
+
+        function editItem (item) {
+            vm.activeItem = item;
+            tempItem = angular.copy(item);
+            vm.editModal.show();
+        }
+
+        function save (item) {
+            vm.totalAssets += (vm.activeItem.quantity * vm.activeItem.unitCost) 
+
+            if (tempItem)
+                vm.totalAssets -= (tempItem.quantity * tempItem.unitCost);
+
+            if (!vm.activeItem.id) {
+                vm.activeItem.id = Math.random() * 100;
+                vm.items.push(vm.activeItem);
+            }
+            vm.activeItem = null;
+            vm.editModal.hide();
+        }
+
+        function cancel () {
+            if (vm.activeItem) {
+                vm.activeItem.name = tempItem.name;
+                vm.activeItem.quantity = tempItem.quantity;
+                vm.activeItem.unitCost = tempItem.unitCost;
+                vm.activeItem.linkInventory = tempItem.linkInventory;
+                vm.activeItem = null;
+            }
+
+            vm.editModal.hide();
+        }
+
+        function addNewItem () {
+            vm.editModal.show();
+        }
+
+        init();
     }
 
     function ExpensesController () {
