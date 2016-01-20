67059fa3355941bb760de184eb4a6b2a4a7bcf68
nkeeslin <nathan.keesling@gmail.com>
2016-01-19 17:08:49 -0700

    Add database Inventory
    Link inventory and product page

--- MicrobusinessManager/www/js/app.js
+++ MicrobusinessManager/www/js/app.js
@@ -36,6 +36,7 @@
 			}
 		})
 		.state('app.products', {
+			cache: false,
 			url: '/products',
 			views: {
 				'menuContent': {
@@ -43,15 +44,79 @@
 					controller: 'ProductsController',
 					controllerAs: 'products'
 				}
+			},
+			resolve: {
+				productItems: function (Database, $q) {
+					var deferred = $q.defer();
+					var promises = [];
+					return Database.select('product').then(function (response) {
+						var items = {};
+
+						if (response.rows.length === 0) {
+							deferred.resolve();
+						}
+
+					    for (var i = response.rows.length - 1; i >= 0; i--) {
+					        var item = response.rows.item(i);
+					        item.price = Number(item.price);
+					        item.linkInventory = item.inventoryid !== null && item.inventoryid !== undefined;
+					        items[item.id] = item;
+					        if (item.linkInventory) {
+					        	promises.push(Database.select('inventory', item.inventoryid).then(function (inventory) {
+					        		items[inventory.rows.item(0).productid].cost = Number(inventory.rows.item(0).cost);
+					        		items[inventory.rows.item(0).productid].quantity = inventory.rows.item(0).quantity;
+					        	}));
+					        } 
+					    };
+					    return $q.all(promises).then(function () {
+					    	return Object.keys(items).map(function (key) {
+					    		return items[key];
+					    	});
+					    });
+					});
+				}
 			}
 		})
 		.state('app.inventory', {
+			cache: false,
 			url: '/inventory',
 			views: {
 				'menuContent': {
 					templateUrl: 'templates/inventory.html',
 					controller: 'InventoryController',
 					controllerAs: 'inventory'
+				}
+			},
+			resolve: {
+				inventoryItems: function (Database, $q) {
+					var deferred = $q.defer();
+					return Database.select('inventory').then(function (response) {
+						var items = {};
+
+						if (response.rows.length === 0) {
+							deferred.resolve();
+						}
+
+					    for (var i = response.rows.length - 1; i >= 0; i--) {
+					        var item = response.rows.item(i);
+					        item.cost = Number(item.cost);
+					        item.linkProduct = item.productid !== null && item.productid !== undefined;
+					        if (item.linkProduct) {
+					        	Database.select('product', item.productid).then(function (product) {
+					        		items[product.rows.item(0).inventoryid].price = Number(product.rows.item(0).price);
+					        		deferred.resolve();
+					        	});
+					        } else {
+					        	deferred.resolve();
+					        }
+					        items[item.id] = item;
+					    };
+					    return deferred.promise.then(function () {
+					    	return Object.keys(items).map(function (key) {
+					    		return items[key];
+					    	});
+					    });
+					});
 				}
 			}
 		})
