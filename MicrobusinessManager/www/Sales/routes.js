4174203ba75d7c0f11859cecb7cc3d4f51c4773b
Travis Nuttall <tuttall@gmail.com>
2016-01-27 11:43:38 -0700

    get inventory limit if applicable

--- MicrobusinessManager/www/Sales/routes.js
+++ MicrobusinessManager/www/Sales/routes.js
@@ -1,34 +1,45 @@
 (function () {
-	angular.module('app.sales')
-	.config(config);
+  angular.module('app.sales')
+  .config(config);
 
-	function config ($stateProvider) {
-		$stateProvider
-		.state('app.sales', {
-			url: '/sales',
-			cache: false,
-			views: {
-				'menuContent': {
-					templateUrl: 'Sales/templates/sales.html',
-					controller: 'SalesController',
-					controllerAs: 'sales'
-				}
-			},
-			resolve: {
-				products: function products (Database) {
-					var items = [];
-					return Database.select('product')
-					.then(function (response) {
-						for (var i = response.rows.length - 1; i >= 0; i--) {
-							var item = response.rows.item(i);
-							item.price = Number(item.price);
-							item.count = 0;
-							items.push(item);
-						}
-						return items;
-					});
-				}
-			}
-		})
-	}
+  function config ($stateProvider) {
+    $stateProvider
+    .state('app.sales', {
+      url: '/sales',
+      cache: false,
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/sales.html',
+          controller: 'SalesController',
+          controllerAs: 'sales'
+        }
+      },
+      resolve: {
+        products: function products (Database) {
+          var items = [];
+          return Database.select('product')
+          .then(function (response) {
+            for (var i = response.rows.length - 1; i >= 0; i--) {
+              var item = response.rows.item(i);
+              item.price = Number(item.price);
+              if (item.inventoryid) {
+                Database.select('inventory', item.inventoryid)
+                  .then(function (response) {
+                    var inventoryItem = response.rows.item(0);
+                    item.limit = inventoryItem.quantity;
+                    item.count = 0;
+                    items.push(item);
+                  });
+              }
+              else {
+                item.count = 0;
+                items.push(item);
+              }
+            }
+            return items;
+          });
+        }
+      }
+    });
+  }
 })();
